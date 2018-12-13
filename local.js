458f6dd37d3eaf2dc795ac120a94e89b268f2e4b
Quade <quade@3rdavevents.com>
2018-12-13 12:09:10 -0800

    add overlay-image route

--- local.js
+++ local.js
@@ -4,6 +4,8 @@ const Sharp = require('sharp');
 const Types = require('./src/types');
 const ImageFetcher = require('./src/s3-image-fetcher');
 const ImageResizr = require('./src/image-resizer');
+const ImageOverlayr = require('./src/image-overlayer');
+
 require('dotenv').config();
 
 app.use(bodyParser.json());
@@ -29,6 +31,39 @@ app.get('/resize-image', (req, res) => {
 
   return imageFetcher.fetchImage(fileName)
     .then(data => imageResizr.resize(data.image, size, quality, type))
+    .then(data => {
+      const img = new Buffer(data.image.buffer, 'base64');
+
+      res.writeHead(200, {
+        'Content-Type': data.contentType
+      });
+      res.end(img);
+    })
+    .catch(error => {
+      console.error('Error:', error);
+      res.status(400).send(error.message || error);
+    });
+});
+
+
+app.get('/overlay-image', async (req, res) => {
+  const imageFetcher = new ImageFetcher(process.env.BUCKET);
+  const imageOverlayr = new ImageOverlayr(Types, Sharp);
+
+  const fileName = req.query && req.query.f;
+  const overlay = req.query && req.query.o;
+  const gravity = req.query && req.query.g;  
+  const top = req.query && req.query.x;
+  const left = req.query && req.query.y;
+  const tile = (req.query && req.query.tile === 'true');
+  const type = req.query && req.query.t;
+
+  console.log('Overlay requested with: GRAVITY:', gravity, ' TOP: ', top, ' LEFT: ', left, ' TILE: ', tile)
+
+  const overlayData = await imageFetcher.fetchImage(overlay);
+
+  return imageFetcher.fetchImage(fileName)
+    .then(data => imageOverlayr.overlay(data.image, overlayData.image, type, gravity, parseInt(top), parseInt(left), tile))
     .then(data => {
       const img = new Buffer(data.image.buffer, 'base64');
 
---
+++ src/image-overlayer.js
@@ -0,0 +1,40 @@
+class ImageOverlayr {
+  constructor(Types, Sharp) {
+    this.types = Types;
+    this.sharp = Sharp;
+  }
+
+  getImageType(type, def = 'webp') {
+    const found = this.types.find(item => item.sharp === type);
+
+    if (!found && type === def) {
+      return { sharp: def, contentType: `image/${def}`};
+    }
+
+    return found || this.getImageType(def, def);
+  }
+
+  overlay(image, overlay, type, gravity, top, left, tile) {
+    if (!image) throw new Error('An Image must be specified');
+    if (!overlay) throw new Error('Image overlay must be specified');
+
+    const sharpType = this.getImageType(type, 'webp');
+  console.log('Overlay requested with: GRAVITY:', gravity, ' TOP: ', top, ' LEFT: ', left, ' TILE: ', tile)
+
+    return new Promise((res, rej) => {
+      this.sharp(new Buffer(image.buffer))
+        .overlayWith(overlay, {gravity: gravity, top: top, left: left, tile: tile})
+        .toBuffer()
+        .then(data => {
+          return res({
+            image: data,
+            contentType: sharpType.contentType,
+          });
+        })
+        .catch(err => rej(err))
+    });
+
+  }
+}
+
+module.exports = ImageOverlayr;
